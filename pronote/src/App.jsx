2a762e6e5740ba53d44a17caa8949fea56fd8564
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-01-20 17:04:08 +0530

    Note Keeping App

---
+++ pronote/src/App.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Editor from './components/Editor';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+         <Editor/>
+    </div>
+  );
+}
+
+export default App;
+
+
